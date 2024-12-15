4429367dfe7bc5bdfcf9dbd2ea9cada5780ec556
jamie-codez <cruiseomondi90@gmail.com>
2024-12-16 02:30:36 +0300

    Clean bill of health lock files

--- frontend/src/app/login/login.page.tsx
+++ frontend/src/app/login/login.page.tsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
                          <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
                              <GalleryVerticalEnd className="size-4"/>
                          </div>
-                         Laundro Inc.
+                         Oyaso Laundry Store
                      </a>
                      <LoginForm/>
                  </div>
